7c17a369ac6271cc8fd3381b0acc1845636d8f33
Evan Freeze <efreeze@lyft.com>
2019-08-06 21:34:08 -0500

    update app

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,40 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Router } from '@reach/router'
+import { createGlobalStyle } from 'styled-components'
 
 import Home from './Home.jsx';
-import Projects from './Projects.jsx';
+import WikipediaViewer from './projects/WikipediaViewer.jsx';
+import ITunesSearcher from './projects/ITunesSearcher.jsx';
 
 const App = () => (
-    <Router>
-        <Home path="/" />
-        <Projects path="/projects" />
-    </Router>
+    <>
+        <GlobalStyles />
+        <Router>
+            <Home path="/" />
+            <WikipediaViewer path="/projects/wikipedia-search" />
+            <ITunesSearcher path="/projects/itunes-preview" />
+        </Router>
+    </>
 )
+
+const GlobalStyles = createGlobalStyle`
+    body {
+        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+        margin: 0;
+        height: 100vh;
+    }
+
+    h1 {
+        font-size: 2rem;
+        margin-bottom: 0.3rem;
+    }
+
+    h2 {
+        font-size: 0.9rem;
+        margin: 0;
+        opacity: 0.65;
+    }
+`
 
 render(<App />, document.getElementById('root'))
