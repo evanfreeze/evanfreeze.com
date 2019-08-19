93018d46eea67342ee52155fc8a333bd795fe75c
Evan Freeze <efreeze@lyft.com>
2019-08-18 19:05:11 -0500

    add EasyTimer component

---
+++ src/components/pages/projects/EasyTimer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import FullScreenFrame from '../../common/FullScreenFrame.jsx';
+
+const EasyTimer = () => (
+    <FullScreenFrame
+        title="Easy Timer"
+        url="https://evanfreeze.github.io/easy-timer/"
+    />
+)
+
+export default EasyTimer
