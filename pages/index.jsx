05ef0e667ecca5193381bc17a51cb638a63e6984
evanfreeze <evanfreeze@gmail.com>
2020-09-02 19:04:05 -0500

    update favicon.ico

--- pages/index.jsx
+++ pages/index.jsx
@@ -19,6 +19,7 @@ function HomePage() {
                 <meta charset="UTF-8" />
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
                 <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+                <link rel="icon" type="image/x-icon" href="/favicon.ico" />
                 <title>Evan Freeze</title>
             </Head>
             <main>
