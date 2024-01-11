0c31a971e51aa2ae7e0f2f33744496a56141b8c4
evanfreeze <evanfreeze@gmail.com>
2024-01-11 17:03:32 -0600

    formatting

--- astro.config.ts
+++ astro.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from 'astro/config';
-import netlify from '@astrojs/netlify';
-import react from '@astrojs/react';
+import { defineConfig } from "astro/config"
+import netlify from "@astrojs/netlify"
+import react from "@astrojs/react"
 
 // https://astro.build/config
 export default defineConfig({
-    output: 'server',
-    site: 'https://www.evanfreeze.com',
-    adapter: netlify(),
-    integrations: [react()]
-});
+  output: "server",
+  site: "https://www.evanfreeze.com",
+  adapter: netlify(),
+  integrations: [react()],
+})
--- src/components/ColorBar.tsx
+++ src/components/ColorBar.tsx
@@ -1,86 +1,86 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react"
 
 function ColorBar() {
-    const [color, setColor] = useState('#0e753a');
+  const [color, setColor] = useState("#0e753a")
 
-    useEffect(() => {
-        const colorInterval = setInterval(() => {
-            const nextColor = getRandomColor(color);
-            setColor(nextColor);
-            document.documentElement.style.setProperty('--bar-color', color);
-        }, 2000);
-        return () => clearInterval(colorInterval);
-    }, []);
+  useEffect(() => {
+    const colorInterval = setInterval(() => {
+      const nextColor = getRandomColor(color)
+      setColor(nextColor)
+      document.documentElement.style.setProperty("--bar-color", color)
+    }, 2000)
+    return () => clearInterval(colorInterval)
+  }, [])
 
-    return (
-        <div
-            style={{
-                content: '',
-                display: 'flex',
-                height: '10px',
-                width: '320px',
-                margin: '20px',
-                justifyItems: 'center',
-                borderRadius: '16px',
-                transition: 'background 2s ease-in-out',
-                backgroundColor: color
-            }}
-        />
-    );
+  return (
+    <div
+      style={{
+        content: "",
+        display: "flex",
+        height: "10px",
+        width: "320px",
+        margin: "20px",
+        justifyItems: "center",
+        borderRadius: "16px",
+        transition: "background 2s ease-in-out",
+        backgroundColor: color,
+      }}
+    />
+  )
 }
 
-export default ColorBar;
+export default ColorBar
 
 var COLORS = [
-    '#ffd6e5',
-    '#ffb8d8',
-    '#e281bf',
-    '#8d3a7f',
-    '#ffc6c5',
-    '#ff8f8f',
-    '#ff4c4d',
-    '#c60638',
-    '#ffdcc0',
-    '#ffa481',
-    '#ff7545',
-    '#e0533b',
-    '#fff2bd',
-    '#ffd952',
-    '#ffb13b',
-    '#ed690e',
-    '#e3f6c1',
-    '#6bdfab',
-    '#0dc98b',
-    '#0e753a',
-    '#c3eae3',
-    '#82d9d7',
-    '#10aeba',
-    '#128994',
-    '#dcf3ff',
-    '#aed8f7',
-    '#81bef0',
-    '#0f7abc',
-    '#d1d7f4',
-    '#648fff',
-    '#036aff',
-    '#1850a5',
-    '#c8cbfc',
-    '#8f91ff',
-    '#785ef0',
-    '#5245c2'
-];
+  "#ffd6e5",
+  "#ffb8d8",
+  "#e281bf",
+  "#8d3a7f",
+  "#ffc6c5",
+  "#ff8f8f",
+  "#ff4c4d",
+  "#c60638",
+  "#ffdcc0",
+  "#ffa481",
+  "#ff7545",
+  "#e0533b",
+  "#fff2bd",
+  "#ffd952",
+  "#ffb13b",
+  "#ed690e",
+  "#e3f6c1",
+  "#6bdfab",
+  "#0dc98b",
+  "#0e753a",
+  "#c3eae3",
+  "#82d9d7",
+  "#10aeba",
+  "#128994",
+  "#dcf3ff",
+  "#aed8f7",
+  "#81bef0",
+  "#0f7abc",
+  "#d1d7f4",
+  "#648fff",
+  "#036aff",
+  "#1850a5",
+  "#c8cbfc",
+  "#8f91ff",
+  "#785ef0",
+  "#5245c2",
+]
 
 function getRandomColor(currentValue: string) {
-    var maxIndex = COLORS.length - 1;
-    var random = Math.random() * maxIndex;
-    var minValue = Math.min(random, maxIndex);
-    var idx = Math.round(minValue);
-    if (currentValue === COLORS[idx]) {
-        if (idx === maxIndex) {
-            idx -= 1;
-        } else {
-            idx += 1;
-        }
+  var maxIndex = COLORS.length - 1
+  var random = Math.random() * maxIndex
+  var minValue = Math.min(random, maxIndex)
+  var idx = Math.round(minValue)
+  if (currentValue === COLORS[idx]) {
+    if (idx === maxIndex) {
+      idx -= 1
+    } else {
+      idx += 1
     }
-    return COLORS[idx];
+  }
+  return COLORS[idx]
 }
